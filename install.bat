fadfc33d85a2fee2a7ec542829c3bc7ea3b8d9e2
Vladimir Gladkov (office-arch) <vovkos@gmail.com>
2019-05-16 16:03:33 +0800

    attempt to build 3.5.2

--- install.bat
+++ install.bat
@@ -2,7 +2,7 @@
 
 :: . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
 
-:: Delete unnecessary packages
+:: delete unnecessary packages
 
 dir \
 
@@ -15,7 +15,7 @@ rd /S /Q "c:\qt"
 
 :: . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
 
-:: Get rid of annoying Xamarin build warnings
+:: get rid of annoying Xamarin build warnings
 
 if exist "c:\Program Files (x86)\MSBuild\14.0\Microsoft.Common.targets\ImportAfter\Xamarin.Common.targets" (
 	del "c:\Program Files (x86)\MSBuild\14.0\Microsoft.Common.targets\ImportAfter\Xamarin.Common.targets"
@@ -27,27 +27,26 @@ if exist "c:\Program Files (x86)\MSBuild\4.0\Microsoft.Common.targets\ImportAfte
 
 :: . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
 
-:: Download LLVM
+:: download LLVM
 
 appveyor DownloadFile %LLVM_DOWNLOAD_URL% -FileName %APPVEYOR_BUILD_FOLDER%\%LLVM_DOWNLOAD_FILE%
 7z x -y %APPVEYOR_BUILD_FOLDER%\%LLVM_DOWNLOAD_FILE% -o%APPVEYOR_BUILD_FOLDER%
 7z x -y %APPVEYOR_BUILD_FOLDER%\llvm-%LLVM_VERSION%.src.tar -o%APPVEYOR_BUILD_FOLDER%
 ren %APPVEYOR_BUILD_FOLDER%\llvm-%LLVM_VERSION%.src llvm
 
-:: patch CMakeLists.cmake to always build and install llvm-config
-
-echo "set_target_properties(llvm-config PROPERTIES EXCLUDE_FROM_ALL FALSE)" >> llvm/CMakeLists.cmake
-echo "install(TARGETS llvm-config RUNTIME DESTINATION bin)" >> llvm/CMakeLists.cmake
-
-:: patch AddLLVM.cmake to also install PDBs on Debug builds
+:: on Debug builds:
+:: - patch CMakeLists.cmake to always build and install llvm-config
+:: - patch AddLLVM.cmake to also install PDBs on Debug builds
 
 if "%CONFIGURATION%" == "Debug" (
+	echo "set_target_properties(llvm-config PROPERTIES EXCLUDE_FROM_ALL FALSE)" >> llvm/CMakeLists.cmake
+	echo "install(TARGETS llvm-config RUNTIME DESTINATION bin)" >> llvm/CMakeLists.cmake
 	perl patch-add-llvm.pl llvm\cmake\modules\AddLLVM.cmake
 )
 
 :: . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
 
-:: Download Clang
+:: download Clang
 
 :: appveyor DownloadFile %CLANG_DOWNLOAD_URL% -FileName %APPVEYOR_BUILD_FOLDER%\%CLANG_DOWNLOAD_FILE%
 :: 7z x -y %APPVEYOR_BUILD_FOLDER%\%CLANG_DOWNLOAD_FILE% -o%APPVEYOR_BUILD_FOLDER%
